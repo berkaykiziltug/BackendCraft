784723e567afebf649fd9eaa5243bb5452dc45e0
Berkay <berkaykiziltug98@gmail.com>
2023-12-26 20:26:19 +0300

    10.gun

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        //degisken olmasina ragmen buyuk harfle yazdik cunku public. Publicler pascal case yazilir. Bu bir field.
+        public static string ProductAdded = "Urun Eklendi.";
+        public static string ProductNameInvalid = "Urun ismi gecersiz";
+        public static string ProductsListed = "Urunler Listelendi";
+        public static string MaintenanceTime = "Maintenance";
+    }
+}
---
+++ Core/Utilities/Results/SuccessResult.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.Results
+{
+    public class SuccessResult:Result
+    {
+        //base in 2 parametreli olan constructorini calistir demek.
+        public SuccessResult(string message) : base(true, message)
+        {
+            
+        }
+
+        //mesaj vermek istemiyorsak direkt olarak base class'in tek parametreli, success'i true olan constructoru calistiririz.
+        public SuccessResult() : base(true)
+        {
+
+        }
+    }
+}
